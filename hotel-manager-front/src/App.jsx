c7675bed6b2ec540a0021432d5607e1fb64de830
Aida Mina <aida.mina@correounivalle.edu.co>
2022-03-15 19:42:20 -0500

    Merge rama-aida

--- hotel-manager-front/src/App.jsx
+++ hotel-manager-front/src/App.jsx
@@ -12,19 +12,7 @@ import VerReserva from './pages/reservas/ver-reserva/VerReserva';
 import Reserva from './components/Reserva';
 import Consultas from './components/Consultas';
 import Prueba from './components/Prueba';
-<<<<<<< HEAD
 import Cargando from './components/componentes-generales/Cargando';
-=======
-import ModuloUsuarios from './pages/ModuloUsuarios';
-import ConsultarReserva from './components/ConsultarReserva';
-import ModificarReservacion from './components/modificar-reserva/ModificarReservacion';
-import ModuloInventario from './pages/ModuloInventario';
-import VerProductosServiciosCliente from './components/modulo-pago/VerProductosServiciosCliente';
-import RegistrarProducto from './components/modulo-inventario/RegistrarProducto';
-import VerAgregarProductosServiciosCliente from './components/modulo-pago/VerAgregarProductosServiciosCliente';
-import DatosOperacion from './components/modulo-pago/DatosOperacion';
-import ModuloPago from './pages/ModuloPago';
->>>>>>> rama-aida
 
 export function App() {
   return(
@@ -43,11 +31,9 @@ export function App() {
         <Route exact path='/reserva' element={ <Reserva /> } />
         <Route exact path='/consultas' element={ <Consultas /> } />
         <Route exact path='/prueba' element={ <Prueba /> } />
-<<<<<<< HEAD
         <Route exact path='/cargando' element={ <Cargando /> } />
         
 
-=======
         <Route exact path='/consultarReservas' element={ <ConsultarReserva /> } />
          <Route exact path='/modificarreserva' element={ <ModificarReservacion /> } />
          <Route exact path='/inventario' element={ <ModuloInventario /> } />
@@ -56,7 +42,6 @@ export function App() {
          <Route exact path='/pagoserviciosproductoscliente' element={ <VerProductosServiciosCliente  /> } />
          <Route exact path='/pagoagregarserviciosproductoscliente' element={ <VerAgregarProductosServiciosCliente  /> } />
          <Route exact path='/realizarpago' element={ <DatosOperacion /> } />
->>>>>>> rama-aida
         
       </Routes>
     </BrowserRouter>
